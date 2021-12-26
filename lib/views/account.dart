b5c81002c193a147de1ca562a2dd71dc7f2ef09c
saroj-007 <sarojbelbase007@gmail.com>
2021-12-26 18:43:18 +0545

    upgrade file of account.dart

--- lib/views/account.dart
+++ lib/views/account.dart
@@ -7,14 +7,124 @@ class FeePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Account Details",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+        centerTitle: true,
+        title: const Text(
+          "Account Details",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12),
+        child: Column(
+          children: [
+            // User Receipt details
+            Container(
+              height: 125,
+              width: 450,
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                border: Border.all(
+                  color: Colors.cyan,
+                  width: 2,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Date and Receipt Number
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Date:",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "Receipt No:",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Course and level
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "Amount:",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )),
+            SizedBox(
+              height: 20,
+            ),
+
+            // User Balance details
+            Container(
+              height: 125,
+              width: 450,
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                border: Border.all(
+                  color: Colors.cyan,
+                  width: 2,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Total due and total paid
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Total Due:",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "Total Paid:",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Balance
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "Balance:",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
